abe43b9587d8a4f4832e8c6d7adeb7ddaf561b59
Thomas Palmqvist <thomas_palmqvist@live.se>
2022-11-23 16:06:52 +0100

    adding scss

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 
-// function LanguageChange() {
-//   function myFunction() {
-//     document.getElementById("myDropdown").classList.toggle("show");
-//   }
-//   return (
-//     <div className="dropdown">
-//       <button onClick={myFunction} class="dropbtn">Dropdown</button>
-//       <div id="myDropdown" className="dropdown-content">
-//         <a href="#home">Home</a>
-//         <a href="#about">About</a>
-//         <a href="#contact">Contact</a>
-//       </div>
-//     </div>
-//   )
-// }
+function LanguageChange() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+  return (
+    <div className="dropdown">
+      <button onClick={handleOpen}>Dropdown</button>
+      {open ? (<ul><li>Swedish</li><li>English</li></ul>) : (null)}
+    </div>
+  )
+}
 
 function Navbar() {
   return (
@@ -25,7 +24,12 @@ function Navbar() {
           <button><li>Home</li></button>
         </ul>
         <input type="text" placeholder='Search IMDb...' />
-        <button type="submit">SEARCH</button>
+        <button type="button">SEARCH</button>
+        <button type="button">Watchlist</button>
+        <button type="button">Sign in</button>
+        <div className='language-container'>
+          <LanguageChange />
+        </div>
       </nav>
     </div>
   )
