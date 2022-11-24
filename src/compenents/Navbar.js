77d29500e7ce2a30b421e967703163740fd54b58
Thomas Palmqvist <thomas_palmqvist@live.se>
2022-11-25 00:19:06 +0100

    etc

--- src/compenents/Navbar.js
+++ src/compenents/Navbar.js
@@ -1,28 +1,35 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { Home } from '../pages/Home';
-import { SingleMovie } from '../pages/SingleMovie';
-import { TopMovies } from '../pages/TopMovies';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { SingleMovie } from "../pages/SingleMovie";
+import { TopMovies } from "../pages/TopMovies";
+import icon2 from "../assets/svg/menu2.svg";
 
 export default function Navbar() {
 
   return (
     <>
-      <div className='navbar-container'>
+      <div className="navbar-container">
         <nav>
-          <button className='btn btn-navbar'>Home</button>
-          <input type="text" placeholder='Search IMDb...' />
-          <button className='btn btn-search' type="button">SEARCH</button>
-          <button className='btn btn-navbar' type="button">Watchlist</button>
-          <button className='btn btn-navbar' type="button">Sign in</button>
+          <button className="btn btn-navbar">Home</button>
+          <button className="btn btn-menu">
+            <div className="hamburger">
+              <img src={icon2} alt="menu icon"></img>
+            </div>
+            Menu
+          </button>
+          <input type="text" placeholder="Search IMDb..." />
+          <button className="btn btn-search" type="button">SEARCH</button>
+          <button className="btn btn-navbar" type="button">Watchlist</button>
+          <button className="btn btn-navbar" type="button">Sign in</button>
         </nav>
-        <div className='language-container'>
+        <div className="language-container">
         </div>
       </div>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='../pages/SingleMovies' element={<SingleMovie />} />
-        <Route path='../pages/TopMovies' element={<TopMovies />} />
+        <Route path="/" element={<Home />} />
+        <Route path="../pages/SingleMovies" element={<SingleMovie />} />
+        <Route path="../pages/TopMovies" element={<TopMovies />} />
       </Routes>
     </>
   )
