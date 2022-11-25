26e66dc8fe5fa3ed610f17e6dc83e08084553f4a
Thomas Palmqvist <thomas_palmqvist@live.se>
2022-11-25 19:39:32 +0100

    Done with the navbar styling

--- src/compenents/Navbar.js
+++ src/compenents/Navbar.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { SingleMovie } from "../pages/SingleMovie";
 import { TopMovies } from "../pages/TopMovies";
 import icon2 from "../assets/svg/menu2.svg";
+import logo from "../assets/svg/IMDB_Logo_2016.svg";
+import search_icon from "../assets/svg/search_icon.svg";
+import IMDb_PRO from "../assets/svg/IMDb_PRO.svg";
+import watchlist_icon from "../assets/svg/watchlist_icon.svg";
 
 export default function Navbar() {
 
@@ -11,7 +15,7 @@ export default function Navbar() {
     <>
       <div className="navbar-container">
         <nav>
-          <button className="btn btn-navbar">Home</button>
+          <Link to="/"><img src={logo} className="logo" alt="IMDb logotype" /></Link>
           <button className="btn btn-menu">
             <div className="hamburger">
               <img src={icon2} alt="menu icon"></img>
@@ -19,9 +23,13 @@ export default function Navbar() {
             Menu
           </button>
           <input type="text" placeholder="Search IMDb..." />
-          <button className="btn btn-search" type="button">SEARCH</button>
-          <button className="btn btn-navbar" type="button">Watchlist</button>
-          <button className="btn btn-navbar" type="button">Sign in</button>
+          <button className="btn btn-search"><img src={search_icon} alt="search icon" /></button>
+          <button className="btn btn-imdb-pro"><img src={IMDb_PRO} alt="IMDb Pro logo" /></button>
+          <div className="seperator-bar"></div>
+          <div className="button-container">
+            <button className="btn btn-navbar" type="button"><img src={watchlist_icon} alt="icon of a gravestone" />Watchlist</button>
+            <button className="btn btn-navbar" type="button">Sign in</button>
+          </div>
         </nav>
         <div className="language-container">
         </div>
